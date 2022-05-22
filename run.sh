4dae284901da93ef4accecc1071004106ac6f91d
ian <dwetham2@gmail.com>
2022-05-22 10:22:26 +0000

    meh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #export PATH=$PATH:/home/ec2-user/ffmpeg
-python3 -m venv virtenv
-source virtenv/bin/activate
+#python3 -m venv virtenv
+#source virtenv/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 python3 main.py
